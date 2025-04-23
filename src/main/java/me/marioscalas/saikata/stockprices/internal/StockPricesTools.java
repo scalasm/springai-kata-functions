0551004a6bf834a4adbacbd0d7171b03eb95173d
Mario Scalas <mario.scalas@hmhco.com>
2025-04-23 11:20:38 +0100

    Added Stock price example

---
+++ src/main/java/me/marioscalas/saikata/stockprices/internal/StockPricesTools.java
@@ -0,0 +1,24 @@
+package me.marioscalas.saikata.stockprices.internal;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Component;
+
+import me.marioscalas.saikata.stockprices.internal.StockPricesClient.StockPricesRequest;
+import me.marioscalas.saikata.stockprices.internal.StockPricesClient.StockPricesResponse;
+
+@Component
+public class StockPricesTools {
+    private final StockPricesClient stockPricesClient;
+
+    public StockPricesTools(StockPricesClient stockPricesClient) {
+        this.stockPricesClient = stockPricesClient;
+    }
+
+    @Tool(
+        name = "getStockPrice",
+        description = "Get the stock price for a given ticker."
+    )
+    public StockPricesResponse getStockPrice(StockPricesRequest request) {
+        return stockPricesClient.getStockPrice(request);
+    }       
+}
